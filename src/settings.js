47e18564631fdff86fcd5ce3281031d60b596a9b
lx <247059750@qq.com>
2022-04-24 17:23:26 +0800

    初始化完毕,进行基本的配置删除不需要的内容,理解项目的结构文件

---
+++ src/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: '后台管理项目', // 配置网页的标题
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, // 配置顶部导航是否固定
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false // 配置左侧导航顶部是否显示logo
+}
